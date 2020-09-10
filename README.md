43488014bef6fd6e0f90d92b1a37c16c2fec386e
Morwran <mat3x@mail.ru>
2020-09-10 13:26:30 +0300

    fix run.py

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 Команда для запуска:
-./run.py path_srec
+./run.py path_srec addr
 
-где path_srec - путь к образу .srec
+где path_srec - путь к образу .srec,
+addr - адрес ПЛИС
 
 Пример:
 
-./run.py /home/kvb/prj/v40/fpga/xilinx13_4/fpga_v2/embed2/embed2/Debug/embed2.srec
+./run.py /home/kvb/prj/v40/fpga/xilinx13_4/fpga_v2/embed2/embed2/Debug/embed2.srec f0
 
 При успешном запуске скрипта в окне терминала ввести команду write и нажать кнопку Enter
 
--- run.py
+++ run.py
@@ -7,26 +7,28 @@ import sys, os
 from terminal import Terminal
 from write_image import WriteImage
 
-port = "/dev/ttyUSB4"
+port = "/dev/ttyUSB0"
 baudrate=115200
 stopbits=1
 parity="N"
 bytesizes=8
 
-ADDR_HW="f1"
+#ADDR_HW="f0"
 
 if __name__ =='__main__':
 
-	if len (sys.argv) > 1:
+	if len (sys.argv) == 2:
 		img_path=str(sys.argv[1])
 		if(os.path.isfile(img_path)):
 			print img_path
 			#WriteImage(image_path=img_path,ADDR_HW=ADDR_HW)
+			addr_hw=str(sys.argv[2])
+			print addr_hw
 			serObj = Terminal(
 				port=port,baudrate=baudrate,
 				stopbits=stopbits,parity=parity,
 				bytesizes=bytesizes,
-				ADDR_HW=ADDR_HW,
+				ADDR_HW=addr_hw,
 				image_path=img_path)
 			serObj.close()
 			serObj.open()
